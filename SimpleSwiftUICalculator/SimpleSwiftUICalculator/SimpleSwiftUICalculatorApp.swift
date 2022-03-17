f3442597e30a49074a67417d308acb0beaeb9a27
Roshan Mykoo <roshanjmykoo@gmail.com>
2022-03-17 17:08:45 -0400

    First Commit, created project for github deployment

---
+++ SimpleSwiftUICalculator/SimpleSwiftUICalculator/SimpleSwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSwiftUICalculatorApp.swift
+//  SimpleSwiftUICalculator
+//
+//  Created by Roshan Mykoo on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
