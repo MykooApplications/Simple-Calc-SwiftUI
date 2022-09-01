6d9705d69d2c95174a1ad49544f06211522716bf
Roshan Mykoo <roshanjmykoo@gmail.com>
2022-09-01 18:29:56 -0400

    Calculator has calculation and order bugs

--- SimpleSwiftUICalculator/SimpleSwiftUICalculator/Model/Calculator.swift
+++ SimpleSwiftUICalculator/SimpleSwiftUICalculator/Model/Calculator.swift
@@ -9,37 +9,31 @@ import Foundation
 
 class Calculator {
     var numberDisplayed: Double = 0
-    
-    //var operation = 0
-    
     var currentCalculation: Double = 0 //first number
     var currentInput: Double = 0       //last number
-    
     
     public func add(currentCalculation first: Double, currentInput second: Double) {
         self.currentCalculation = first + second
     }
     
-    public func subtract() {
-        
+    public func subtract(firstInput first: Double, secondInput second: Double) {
+        self.currentCalculation = first - second
     }
     
-    public func multiply() {
-        
+    public func multiply(firstInput first: Double, secondInput second: Double) {
+        self.currentCalculation = first * second
     }
     
-    public func divide() {
-        
+    public func divide(firstInput first: Double, secondInput second: Double) {
+        self.currentCalculation = first / second
     }
     
     public func enterButtonPressed() {
         printCurrentCalculation()
     }
     
-    
     func printCurrentCalculation() {
         print(currentCalculation)
     }
-    
     
 }
